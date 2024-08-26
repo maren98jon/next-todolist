9e0631a2e07893c594c0b4d4987746bb831dde3d
Maren <maren@develov.com>
2024-08-26 19:00:49 +0200

    Fin seccion 10

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -40,3 +40,17 @@ export async function POST(request: Request) {
 
 
 }
+
+export async function DELETE(request: Request) {
+
+    try {
+
+        const todo = await prisma.todo.deleteMany({where: {complete: true}});
+
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json(error, {status: 400});
+    }
+
+
+}
---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,32 @@
+import prisma from "@/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+
+
+export const metadata = {
+ title: 'Listado de Todos',
+ description: 'Listado de Todos',
+};
+
+export default async function RestTodosPage() {
+
+  // useEffect(() => {
+  //   fetch('/api/todos')
+  //     .then( resp => resp.json() )
+  //     .then( console.log );
+
+  // }, [])
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc'}});
+
+  return (
+    <div>
+      {/* TODO: formulario para agregar TODOs */}
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+      
+      <TodosGrid todos={ todos } />
+    </div>
+  );
+}
