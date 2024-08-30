df9012c78bfdf0af9281214af3b98b255896c9a8
Maren <maren@develov.com>
2024-08-30 17:39:37 +0200

    Fin seccion 12

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 
 export { Sidebar } from './Sidebar';
 export { SidebarItem } from './SidebarItem';
+export { TabBar } from './TabBar';
 export { TopMenu } from './TopMenu';
 export { WidgetItem } from './WidgetItem';
